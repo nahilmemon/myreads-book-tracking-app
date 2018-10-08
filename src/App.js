f959d6ec0a346a0466d767d7909c0587932017b6
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-10-08 19:45:19 +0400

    refactor: Create static BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Book from './components/Book.js'
+import BookShelf from './components/BookShelf.js'
 
 class BooksApp extends React.Component {
   state = {
@@ -45,34 +45,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book />
-                      <Book />
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book />
-                      <Book />
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book />
-                      <Book />
-                      <Book />
-                    </ol>
-                  </div>
-                </div>
+                <BookShelf />
+                <BookShelf />
+                <BookShelf />
               </div>
             </div>
             <div className="open-search">
