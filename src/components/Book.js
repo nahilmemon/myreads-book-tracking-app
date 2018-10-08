3e20377ce8eebbca40f001cba45d2563a1e3e22e
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-10-08 19:36:40 +0400

    refactor: Create static book component

---
+++ src/components/Book.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
+          <div className="book-authors">J.K. Rowling</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Book
