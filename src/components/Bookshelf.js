d5e0b8edf0bcf3bcdf64830f7dea34e8b30496b9
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-10-18 02:13:29 +0400

    fix: Change filename of BookShelf.js to Bookshelf.js
    Make git finally recognize filename change from BookShelf.js to Bookshelf.js (which happened near the beginning of the project) by updating the case sensitivity configuration.

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BookGrid from './BookGrid.js';
+
+class Bookshelf extends Component {
+  static propTypes = {
+    books: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onMoveBookToNewShelf: PropTypes.func.isRequired,
+    shelf: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    displayShelfIcon: PropTypes.bool.isRequired,
+    areLibraryBooksLoaded: PropTypes.bool.isRequired,
+    page: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <section className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        {this.props.books.length <= 0 && this.props.areLibraryBooksLoaded ? (
+          <p className="message">This shelf is empty.</p>
+        ) : (
+          <BookGrid
+            className="bookshelf-books"
+            books={this.props.books}
+            onMoveBookToNewShelf={this.props.onMoveBookToNewShelf}
+            areBooksLoaded={this.props.areLibraryBooksLoaded}
+            displayShelfIcon={this.props.displayShelfIcon}
+            page={this.props.page}
+          />
+        )}
+      </section>
+    );
+  }
+}
+
+export default Bookshelf;
